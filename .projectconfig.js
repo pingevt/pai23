742c313806665bdcfd6af31d7a3b01071604eb7f
Pete Inge <pinge@bluecadet.com>
2023-07-04 22:48:55 -0400

    Adding more pages to test!

--- .projectconfig.js
+++ .projectconfig.js
@@ -11,6 +11,98 @@ module.exports = {
     {
       "label": "Home page",
       "path": "",
+    },
+    {
+      "label": "Work",
+      "path": "node/2",
+    },
+    {
+      "label": "About",
+      "path": "node/1",
+    },
+    {
+      "label": "Resume",
+      "path": "node/3",
+    },
+    {
+      "label": "Project: AC ongoing maintence",
+      "path": "node/28",
+    },
+    {
+      "label": "Project: Longwood Gardens",
+      "path": "node/6",
+    },
+    {
+      "label": "Project: Amon Carter - Col Exp / Phase 2",
+      "path": "node/22",
+    },
+    {
+      "label": "Project: Amon Carter - Phase 1",
+      "path": "node/7",
+    },
+    {
+      "label": "Project: Bucknell",
+      "path": "node/27",
+    },
+    {
+      "label": "Project: NHM",
+      "path": "node/32",
+    },
+    {
+      "label": "Project: River Alive - Continuum Wall",
+      "path": "node/33",
+    },
+    {
+      "label": "Sketch: Dealing with Drupal Cache",
+      "path": "node/34",
+    },
+    {
+      "label": "Sketch: Checking field content in a twig file",
+      "path": "node/4",
+    },
+    {
+      "label": "Sketch: Image Color/Data Processor Exploration",
+      "path": "node/19",
+    },
+    {
+      "label": "Sketch: Generative Art with Unity",
+      "path": "node/18",
+    },
+    {
+      "label": "Sketch: Websockets w/ Unity",
+      "path": "node/23",
+    },
+    {
+      "label": "Talks: Drupal (And Maybe Wordpress) Site Content with Airtable",
+      "path": "node/14",
+    },
+    {
+      "label": "Talks: Injecting React into Your Authoring Experience",
+      "path": "node/15",
+    },
+    {
+      "label": "Talks: Five Minute Drupal Site - Speeding up our Drupal Dev Time",
+      "path": "node/16",
+    },
+    {
+      "label": "Talks: Realtime JavaScript Applications (PWAs and Drupal) - Drupaldelphia",
+      "path": "node/26",
+    },
+    {
+      "label": "Talks: Drupal 8 - The Missing Parts",
+      "path": "node/13",
+    },
+    {
+      "label": "Writings: Pantheon and GitHub Actions",
+      "path": "node/8",
+    },
+    {
+      "label": "Writings: Drupal Programmatic Translations R&D",
+      "path": "node/9",
+    },
+    {
+      "label": "Writings: Google Drive, Node.js, and a custom Electron app",
+      "path": "node/10",
     }
   ]
 }
