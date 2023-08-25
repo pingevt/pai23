5bc38d13d939ce8a9684b51a6356fd29f500acaf
Pete Inge <pinge@bluecadet.com>
2023-08-25 16:08:00 -0400

    Add in lazy load for images

---
+++ web/themes/custom/pai/assets/src/js/lazy_load_images.js
@@ -0,0 +1,59 @@
+// eslint-disable-next-line no-unused-vars
+(($, Drupal, drupalSettings) => {
+
+  let contentObserverOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1.0,
+    passive: true,
+  }
+
+  const ajaxContentObserverCallback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        let elem = entry.target;
+
+        elem.src = elem.dataset.src;
+        elem.srcset = elem.dataset.srcset;
+        elem.classList.add("loaded");
+
+        parent = elem.parentNode;
+        parent.style = "";
+        parent.classList.add('m-loaded')
+
+        imgLazyLoadObserver.unobserve(elem);
+      }
+
+    });
+  }
+
+  let imgLazyLoadObserver = new IntersectionObserver(ajaxContentObserverCallback, contentObserverOptions);
+
+  Drupal.behaviors.ac_utilities_content_ajaxing = {
+    attach: function (context, settings) {
+      const imgEls = context.querySelectorAll(".media.media--view-mode-teaser img:not(.observing)");
+
+      imgEls.forEach(el => {
+        imgLazyLoadObserver.observe(el);
+        el.classList.add("observing");
+      });
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// eslint-disable-next-line no-undef
+})(jQuery, Drupal, drupalSettings);
