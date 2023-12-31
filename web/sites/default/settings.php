a6aeec3027fedef58af6b363c69a465633febd28
Pete Inge <pete.inge@gmail.com>
2023-12-30 22:13:52 -0500

    Add in config split
    #12

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -1,14 +1,16 @@
 <?php
 
 /**
+ * @file
  * Load services definition file.
  */
+
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
 
 /**
  * Include the Pantheon-specific settings file.
  *
- * n.b. The settings.pantheon.php file makes some changes
+ * N.b. The settings.pantheon.php file makes some changes
  *      that affect all environments that this site
  *      exists in.  Always include this file, even in
  *      a local development environment, to ensure that
@@ -26,14 +28,23 @@ $settings['config_sync_directory'] = dirname(DRUPAL_ROOT) . '/config';
  * work better, but will also raise a warning when you
  * install Drupal.
  *
- * https://www.drupal.org/project/drupal/issues/3091285
+ * Https://www.drupal.org/project/drupal/issues/3091285
  */
 // $settings['skip_permissions_hardening'] = TRUE;
 
 /**
- * If there is a local settings file, then include it
+ * If there is a local settings file, then include it.
  */
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
+}
+
+// Config Split Settings.
+$config['config_split.config_split.development']['status'] = TRUE;
+if (defined('PANTHEON_ENVIRONMENT')) {
+  $config['config_split.config_split.development']['folder'] = dirname(DRUPAL_ROOT) . '/config-dev';
+  if (in_array($_ENV['PANTHEON_ENVIRONMENT'], ['live', 'test', 'pr-887'])) {
+    $config['config_split.config_split.development']['status'] = FALSE;
+  }
 }
